a8215361abbaece7e9a81dae3b97fb1c1048fc73
TheJoeCoder <joe@radialbog9.uk>
2020-11-03 20:15:35 +0000

    Made some more progress with SpectateCommand
    Signed-off-by: TheJoeCoder <joeleaver77@gmail.com>

--- src/main/java/uk/radialbog9/spigot/manhunt/SpectateCommand.java
+++ src/main/java/uk/radialbog9/spigot/manhunt/SpectateCommand.java
@@ -1,5 +1,7 @@
 package uk.radialbog9.spigot.manhunt;
 
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,6 +12,7 @@ public class SpectateCommand implements CommandExecutor {
     /**
      * Spectate command
      * <code>/spectate &lt;player&gt;</code>
+     * Allows people who are not in a game to spectate the hunters/runners.
      * @param sender CommandSender command sender
      * @param cmd Command command
      * @param label String label
@@ -17,25 +20,45 @@ public class SpectateCommand implements CommandExecutor {
      * @return boolean always true
      */
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if(sender.hasPermission("manhunt.spectate")) {
-            //permission
-            if(args.length == 0) {
-                //no player specified
-                sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Not enough agruments!"));
-                sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Usage: &c/spectate <player>"));
-            } else if (args.length > 1) {
-                //too many arguments
-                sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Too many arguments!"));
-                sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Usage: &c/spectate <player>"));
+        if(sender instanceof Player) {
+            Player p = (Player) sender;
+            if(sender.hasPermission("manhunt.spectate")) {
+                //permission
+                if(args.length == 0) {
+                    //no player specified
+                    p.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Not enough agruments!"));
+                    p.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Usage: &c/spectate <player>"));
+                } else if (args.length > 1) {
+                    //too many arguments
+                    p.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Too many arguments!"));
+                    p.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Usage: &c/spectate <player>"));
+                } else {
+                    //player given
+                    //check if in game
+                    if(ManhuntVars.isGameStarted()) {
+                        //check the player
+                        Player existingPlayer = Bukkit.getPlayer(args[0]);
+                        if(existingPlayer != null) {
+                            //player exists
+                            if(ManhuntVars.isRunner(existingPlayer) || ManhuntVars.isHunter(existingPlayer)) {
+                                p.setGameMode(GameMode.SPECTATOR);
+                                p.teleport(existingPlayer);
+                                p.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a You are now spectating &c" + existingPlayer.getDisplayName() + "&r&a."));
+                            }
+                        } else {
+                            p.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a That player is not online!"));
+                        }
+                    } else {
+                        p.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a There is no game in progress."));
+                    }
+                }
             } else {
-                //player given
-                //check the player
-                Player existingPlayer = null;
-
+                //no permission
+                sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a You do not have permission to do this!"));
             }
         } else {
-            //no permission
-            sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a You do not have permission to do this!"));
+            //console is player
+            sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Console cannot spectate players!"));
         }
         return true;
     }
