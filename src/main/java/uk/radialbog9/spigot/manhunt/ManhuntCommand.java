5b1e3680da885a2b5b81fa17d3ab19fc45bd5e0a
TheJoeCoder <joeleaver77@gmail.com>
2020-11-14 09:58:50 +0000

    Done quite a few things Added most of the manhunt commands in ManhuntCommand Improved help dialog
    Signed-off-by: TheJoeCoder <joeleaver77@gmail.com>

--- src/main/java/uk/radialbog9/spigot/manhunt/ManhuntCommand.java
+++ src/main/java/uk/radialbog9/spigot/manhunt/ManhuntCommand.java
@@ -1,8 +1,10 @@
 package uk.radialbog9.spigot.manhunt;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class ManhuntCommand implements CommandExecutor {
 
@@ -20,18 +22,135 @@ public class ManhuntCommand implements CommandExecutor {
         } else if (args[0].equalsIgnoreCase("help")) {
             sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Command Help:"));
             sender.sendMessage(Utils.getMsgColor("&a- &c/manhunt help &r&a- &r&eShows help"));
-            sender.sendMessage(Utils.getMsgColor("&a- &c/manhunt hunter <player> &r&a- &r&eSets a player to hunter"));
-            sender.sendMessage(Utils.getMsgColor("&a- &c/manhunt runner <player> &r&a- &r&eSets a player to runner"));
-            sender.sendMessage(Utils.getMsgColor("&a- &c/manhunt remove <player> &r&a- &r&eSets a player to runner"));
-            sender.sendMessage(Utils.getMsgColor("&a- &c/manhunt start &r&a- &r&eStarts the manhunt"));
-            sender.sendMessage(Utils.getMsgColor("&a- &c/manhunt stop &r&a- &r&eStops the game"));
-            sender.sendMessage(Utils.getMsgColor("&a- &c/manhunt list &r&a- &r&eList all runners and hunters"));
+            if(Utils.hasNoManhuntPermissions(sender)) sender.sendMessage(Utils.getMsgColor("&a- You do not have permission for any sub-commands."));
+            if(sender.hasPermission("manhunt.add")) sender.sendMessage(Utils.getMsgColor("&a- &c/manhunt hunter <player> &r&a- &r&eSets a player to hunter"));
+            if(sender.hasPermission("manhunt.add")) sender.sendMessage(Utils.getMsgColor("&a- &c/manhunt runner <player> &r&a- &r&eSets a player to runner"));
+            if(sender.hasPermission("manhunt.remove")) sender.sendMessage(Utils.getMsgColor("&a- &c/manhunt remove <player> &r&a- &r&eRemoves a player from hunter and runner"));
+            if(sender.hasPermission("manhunt.start")) sender.sendMessage(Utils.getMsgColor("&a- &c/manhunt start &r&a- &r&eStarts the manhunt"));
+            if(sender.hasPermission("manhunt.stop")) sender.sendMessage(Utils.getMsgColor("&a- &c/manhunt stop &r&a- &r&eStops the game"));
+            if(sender.hasPermission("manhunt.list")) sender.sendMessage(Utils.getMsgColor("&a- &c/manhunt list &r&a- &r&eList all runners and hunters"));
+            if(sender.hasPermission("manhunt.spectate")) sender.sendMessage(Utils.getMsgColor("&a- &c/spectate <player> &r&a- &r&eSpectates players"));
         } else if (args[0].equalsIgnoreCase("hunter")) {
             if(sender.hasPermission("manhunt.add")) {
-                if(args.length == 1) {
+                if(args.length < 2) {
                     //no player specified
+                    sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Not enough arguments!"));
+                    sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Usage: &c/manhunt hunter <player>"));
+                } else if (args.length > 2) {
+                    //too many players specified
+                    sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Too many arguments!"));
+                    sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Usage: &c/manhunt hunter <player>"));
                 } else {
                     //player specified
+                    //check if game is started
+                    if(!ManhuntVars.isGameStarted()) {
+                        //game is not started, we can change hunters and runners
+                        //check if player exists
+                        Player pl = Bukkit.getPlayer(args[1]);
+                        if(pl != null) {
+                            //player exists
+                            //check if already hunter
+                            if(!ManhuntVars.isHunter(pl)) {
+                                //player isn't hunter already - add them
+                                if(ManhuntVars.isRunner(pl)) ManhuntVars.removeRunner(pl);
+                                ManhuntVars.addHunter(pl);
+                                sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a The player &r&c" + pl.getDisplayName() + "&r&a is now a hunter!"));
+                                pl.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a You are now a hunter!"));
+                            } else {
+                                //already a hunter
+                                sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a The player &r&c" + pl.getDisplayName() + "&r&a is already a hunter!"));
+                            }
+                        } else {
+                            //player does not exist/is not online
+                            sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a The player &r&c" + args[1] + "&r&a is not online!"));
+                        }
+                    } else {
+                        sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a You can not change hunters while the game is ongoing."));
+                    }
+                }
+            } else {
+                //no perm
+                sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a You do not have permission to do this!"));
+            }
+        } else if (args[0].equalsIgnoreCase("runner")) {
+            if(sender.hasPermission("manhunt.add")) {
+                if(args.length < 2) {
+                    //no player specified
+                    sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Not enough arguments!"));
+                    sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Usage: &c/manhunt runner <player>"));
+                } else if (args.length > 2) {
+                    //too many players specified
+                    sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Too many arguments!"));
+                    sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Usage: &c/manhunt runner <player>"));
+                } else {
+                    //player specified
+                    //check if game is started
+                    if(!ManhuntVars.isGameStarted()) {
+                        //game is not started, we can change hunters and runners
+                        //check if player exists
+                        Player pl = Bukkit.getPlayer(args[1]);
+                        if(pl != null) {
+                            //player exists
+                            //check if already runner
+                            if(!ManhuntVars.isRunner(pl)) {
+                                //player isn't runner already - add them
+                                if(ManhuntVars.isHunter(pl)) ManhuntVars.removeHunter(pl);
+                                ManhuntVars.addRunner(pl);
+                                sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a The player &r&c" + pl.getDisplayName() + "&r&a is now a runner!"));
+                                pl.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a You are now a runner!"));
+                            } else {
+                                //already a hunter
+                                sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a The player &r&c" + pl.getDisplayName() + "&r&a is already a runner!"));
+                            }
+                        } else {
+                            //player does not exist/is not online
+                            sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a The player &r&c" + args[1] + "&r&a is not online!"));
+                        }
+                    } else {
+                        sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a You can not change runners while the game is ongoing."));
+                    }
+                }
+            } else {
+                //no perm
+                sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a You do not have permission to do this!"));
+            }
+        } else if (args[0].equalsIgnoreCase("remove")) {
+            if(sender.hasPermission("manhunt.remove")) {
+                if(args.length < 2) {
+                    //no player specified
+                    sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Not enough arguments!"));
+                    sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Usage: &c/manhunt runner <player>"));
+                } else if (args.length > 2) {
+                    //too many players specified
+                    sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Too many arguments!"));
+                    sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a Usage: &c/manhunt runner <player>"));
+                } else {
+                    //player specified
+                    //check if game is started
+                    if(!ManhuntVars.isGameStarted()) {
+                        //game is not started, we can change hunters and runners
+                        //check if player exists
+                        Player pl = Bukkit.getPlayer(args[1]);
+                        if(pl != null) {
+                            //player exists
+                            //check if already runner
+                            if(!ManhuntVars.isRunner(pl)) {
+                                //player isn't runner already - add them
+                                if(ManhuntVars.isHunter(pl)) ManhuntVars.removeHunter(pl);
+                                ManhuntVars.addRunner(pl);
+                                sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a The player &r&c" + pl.getDisplayName() + "&r&a is now a runner!"));
+                                pl.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a You are now a runner!"));
+                            } else {
+                                //already a hunter
+                                sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a The player &r&c" + pl.getDisplayName() + "&r&a is already a runner!"));
+                            }
+                        } else {
+                            //player does not exist/is not online
+                            sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a The player &r&c" + args[1] + "&r&a is not online!"));
+                        }
+                    } else {
+                        sender.sendMessage(Utils.getMsgColor("&6[Manhunt]&r&a You can not change runners while the game is ongoing."));
+                    }
                 }
             } else {
                 //no perm
--- src/main/java/uk/radialbog9/spigot/manhunt/Utils.java
+++ src/main/java/uk/radialbog9/spigot/manhunt/Utils.java
@@ -2,12 +2,30 @@ package uk.radialbog9.spigot.manhunt;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.List;
 
 /**
  * Utilities to use
  */
 public class Utils {
+
+    private static List<String> manhuntPermissions;
+
+    /**
+     * Initiates all permissions and other things in this class
+     */
+    public static void onPlEnable() {
+        manhuntPermissions.add("manhunt.add");
+        manhuntPermissions.add("manhunt.remove");
+        manhuntPermissions.add("manhunt.start");
+        manhuntPermissions.add("manhunt.stop");
+        manhuntPermissions.add("manhunt.list");
+        manhuntPermissions.add("manhunt.spectate");
+    }
+
     /**
      * Shorter way to get chat message colors
      * @param msg String Chat message
@@ -27,5 +45,30 @@ public class Utils {
         for(Player p : Bukkit.getOnlinePlayers()) {
             p.sendMessage(msgSend);
         }
+    }
+
+    /**
+     * Checks for all manhunt permissions
+     * @param s CommandSender
+     * @return boolean if sender has no manhunt permissions
+     */
+    public static boolean hasNoManhuntPermissions(CommandSender s) {
+        boolean hasNoMHP = true;
+        for(String perm : manhuntPermissions) {
+            if(s.hasPermission(perm)) hasNoMHP = false;
+        }
+        return hasNoMHP;
+    }
+    /**
+     * Checks for all manhunt permissions
+     * @param p Player
+     * @return boolean if player has no manhunt permissions
+     */
+    public static boolean hasNoManhuntPermissions(Player p) {
+        boolean hasNoMHP = true;
+        for(String perm : manhuntPermissions) {
+            if(p.hasPermission(perm)) hasNoMHP = false;
+        }
+        return hasNoMHP;
     }
 }
