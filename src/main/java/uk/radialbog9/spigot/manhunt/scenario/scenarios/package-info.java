bca103438e38ad33a9ff75bc43cacdb939f874f2
TheJoeCoder <joe@radialbog9.uk>
2022-05-22 09:34:12 +0100

    Update copyright
    Signed-off-by: TheJoeCoder <joe@radialbog9.uk>

--- src/main/java/uk/radialbog9/spigot/manhunt/scenario/scenarios/package-info.java
+++ src/main/java/uk/radialbog9/spigot/manhunt/scenario/scenarios/package-info.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2020-2022 Radialbog9/TheJoeCoder and contributors.
+ * You are allowed to use this code under the GPL3 license, which allows commercial use, distribution, modification, and licensed works, providing that you distribute your code under the same or similar license.
+ */
+
 /**
  * Contains the standalone scenarios (the ones that don't need other plugins to operate)
  */
--- src/main/java/uk/radialbog9/spigot/manhunt/settings/package-info.java
+++ src/main/java/uk/radialbog9/spigot/manhunt/settings/package-info.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2020-2022 Radialbog9/TheJoeCoder and contributors.
+ * You are allowed to use this code under the GPL3 license, which allows commercial use, distribution, modification, and licensed works, providing that you distribute your code under the same or similar license.
+ */
+
 /**
  * Class for the in-game settings and menu
  */
