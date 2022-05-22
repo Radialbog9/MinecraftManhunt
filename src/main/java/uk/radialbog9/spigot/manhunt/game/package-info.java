90cc60f8b68f2011ce62cf9e8a16c4d40f0a6be5
TheJoeCoder <joe@radialbog9.uk>
2022-05-22 22:21:32 +0100

    Add game package javadoc
    Signed-off-by: TheJoeCoder <joe@radialbog9.uk>

---
+++ src/main/java/uk/radialbog9/spigot/manhunt/game/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2020-2022 Radialbog9/TheJoeCoder and contributors.
+ * You are allowed to use this code under the GPL3 license, which allows commercial use, distribution, modification, and licensed works, providing that you distribute your code under the same or similar license.
+ */
+/**
+ * Package containing the game's main logic
+ */
+package uk.radialbog9.spigot.manhunt.game;
