2f9faa47f8171e470d7a231337de68507a6e4ad5
TheJoeCoder <joe@radialbog9.uk>
2022-07-23 17:28:30 +0100

    Update Copyright Notice
    Signed-off-by: TheJoeCoder <joe@radialbog9.uk>

--- src/main/java/uk/radialbog9/spigot/manhunt/commands/SpectateCommand.java
+++ src/main/java/uk/radialbog9/spigot/manhunt/commands/SpectateCommand.java
@@ -1,6 +1,8 @@
 /*
  * Copyright (c) 2020-2022 Radialbog9/TheJoeCoder and contributors.
- * You are allowed to use this code under the GPL3 license, which allows commercial use, distribution, modification, and licensed works, providing that you distribute your code under the same or similar license.
+ * You are allowed to use this code under the GPL3 license, which allows
+ * commercial use, distribution, modification, and licensed works,
+ * providing that you distribute your code under the same or similar license.
  */
 
 package uk.radialbog9.spigot.manhunt.commands;
