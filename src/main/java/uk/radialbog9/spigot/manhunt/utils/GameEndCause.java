40e2e2eb951d8d8bbcdbb6a286783257864b7424
TheJoeCoder <joe@radialbog9.uk>
2023-08-27 08:52:31 +0100

    Refactor javadoc in GameEndCause.java

--- src/main/java/uk/radialbog9/spigot/manhunt/utils/GameEndCause.java
+++ src/main/java/uk/radialbog9/spigot/manhunt/utils/GameEndCause.java
@@ -9,20 +9,30 @@ package uk.radialbog9.spigot.manhunt.utils;
 
 /**
  * Game Win Causes
- *
- * All causes:
- * RUNNERS_ALL_DIE: When the runners are all dead <br>
- * RUNNERS_KILL_DRAGON: When the runners kill the Ender Dragon <br>
- * ALL_HUNTERS_LEAVE: When all hunters have left the server <br>
- * ALL_RUNNERS_LEAVE: When all runners have left the server <br>
- * TIME_UP: To be used in a gamemode where runners have a certain time to survive <br>
- * ENDED_PREMATURELY: When a game is stopped via a command. <br>
  */
 public enum GameEndCause {
+    /**
+     * When the runners are all dead
+     */
     RUNNERS_ALL_DIE,
+    /**
+     * When the runners kill the Ender Dragon
+     */
     RUNNERS_KILL_DRAGON,
+    /**
+     * When all hunters have left the server
+     */
     ALL_HUNTERS_LEAVE,
+    /**
+     * When all runners have left the server
+     */
     ALL_RUNNERS_LEAVE,
+    /**
+     * When the game timer expires (certain gamemodes)
+     */
     TIME_UP,
+    /**
+     * When the game is stopped with a command
+     */
     ENDED_PREMATURELY
 }
