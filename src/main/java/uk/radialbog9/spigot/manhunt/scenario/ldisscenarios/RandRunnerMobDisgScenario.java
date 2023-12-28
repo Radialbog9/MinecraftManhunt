62a3f353e28ef45b2ee550934e2d91090a6cacff
TheJoeCoder <joe@radialbog9.uk>
2023-12-28 19:43:24 +0000

    Refactor LibsDisguises scenarios
    Signed-off-by: TheJoeCoder <joe@radialbog9.uk>

--- src/main/java/uk/radialbog9/spigot/manhunt/scenario/ldisscenarios/RandRunnerMobDisgScenario.java
+++ src/main/java/uk/radialbog9/spigot/manhunt/scenario/ldisscenarios/RandRunnerMobDisgScenario.java
@@ -7,16 +7,14 @@
 
 package uk.radialbog9.spigot.manhunt.scenario.ldisscenarios;
 
-import me.libraryaddict.disguise.DisguiseAPI;
-import me.libraryaddict.disguise.disguisetypes.Disguise;
-import me.libraryaddict.disguise.disguisetypes.DisguiseType;
-import me.libraryaddict.disguise.disguisetypes.MobDisguise;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import uk.radialbog9.spigot.manhunt.game.GameManager;
-import uk.radialbog9.spigot.manhunt.scenario.*;
+import uk.radialbog9.spigot.manhunt.scenario.Scenario;
+import uk.radialbog9.spigot.manhunt.scenario.ScenarioRunnable;
+import uk.radialbog9.spigot.manhunt.scenario.ScenarioType;
 import uk.radialbog9.spigot.manhunt.utils.DependencySupport;
-import uk.radialbog9.spigot.manhunt.utils.Utils;
+import uk.radialbog9.spigot.manhunt.utils.LibsDisguisesUtils;
 
 @Scenario(ScenarioType.RUNNER_RANDOM_MOB_DISGUISE)
 @ScenarioRunnable
@@ -30,16 +28,7 @@ public class RandRunnerMobDisgScenario extends BukkitRunnable {
                 GameManager.getGame().getActiveScenarios().contains(ScenarioType.RUNNER_RANDOM_MOB_DISGUISE)
         ) {
             for(Player p : GameManager.getGame().getRunners()) {
-                boolean isMobYet = false;
-                DisguiseType disguisetype = null;
-                while (!isMobYet) {
-                    int dis = Utils.getRandomInt(0, DisguiseType.values().length - 1);
-                    disguisetype = DisguiseType.values()[dis];
-                    isMobYet = disguisetype.isMob();
-                }
-                Disguise disguise = new MobDisguise(disguisetype);
-                DisguiseAPI.disguiseEntity(p, disguise);
-                p.sendMessage("You are now a " + disguisetype.toReadable());
+                LibsDisguisesUtils.disguisePlayerRandomMob(p);
             }
         } else {
             this.cancel();
---
+++ src/main/java/uk/radialbog9/spigot/manhunt/utils/LibsDisguisesUtils.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2020-2023 Radialbog9/TheJoeCoder and contributors.
+ *  You are allowed to use this code under the GPL3 license, which allows
+ *  commercial use, distribution, modification, and licensed works,
+ *  providing that you distribute your code under the same or similar license.
+ */
+
+package uk.radialbog9.spigot.manhunt.utils;
+
+import me.libraryaddict.disguise.DisguiseAPI;
+import me.libraryaddict.disguise.disguisetypes.Disguise;
+import me.libraryaddict.disguise.disguisetypes.DisguiseType;
+import me.libraryaddict.disguise.disguisetypes.MobDisguise;
+import org.bukkit.entity.Player;
+
+public class LibsDisguisesUtils {
+    public static void disguisePlayerRandomMob(Player p) {
+        boolean isMobYet = false;
+        DisguiseType disguisetype = null;
+        while (!isMobYet) {
+            int dis = Utils.getRandomInt(0, DisguiseType.values().length - 1);
+            disguisetype = DisguiseType.values()[dis];
+            isMobYet = disguisetype.isMob();
+        }
+        Disguise disguise = new MobDisguise(disguisetype);
+        DisguiseAPI.disguiseEntity(p, disguise);
+        p.sendMessage("You are now a " + disguisetype.toReadable());
+    }
+}
