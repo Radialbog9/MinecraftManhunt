53fe4dfb9656a5279db0a6aa60d4587acaffb4fd
TheJoeCoder <joe@radialbog9.uk>
2024-07-01 23:04:56 +0100

    Add block offset checking for standing and swimming
    Signed-off-by: TheJoeCoder <joe@radialbog9.uk>

--- src/main/java/uk/radialbog9/spigot/manhunt/scenario/scenarios/PoisonousAirScenario.java
+++ src/main/java/uk/radialbog9/spigot/manhunt/scenario/scenarios/PoisonousAirScenario.java
@@ -7,6 +7,8 @@
 
 package uk.radialbog9.spigot.manhunt.scenario.scenarios;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -23,7 +25,23 @@ public class PoisonousAirScenario extends BukkitRunnable {
     public void run() {
         if(GameManager.getGame().isGameStarted() && GameManager.getGame().getActiveScenarios().contains("POISONOUS_AIR")) {
             for(Player player : GameManager.getGame().getPlayers()) {
-                if(!player.getLocation().getBlock().getType().isSolid()) {
+                // Check if the player is swimming or gliding
+                // If they are, we need to check the block they are in
+                // Otherwise, we need to check the block above them (their head)
+                Location checkLocation = player.getLocation().clone().add(0, 1, 0);
+                if(player.isSwimming() || player.isGliding())
+                    checkLocation = player.getLocation();
+
+                // Check if the block is solid, water, or lava
+                Material blockType = checkLocation.getBlock().getType();
+
+                if(!blockType.isSolid() &&
+                        blockType != Material.WATER &&
+                        blockType != Material.LAVA &&
+                        blockType != Material.POWDER_SNOW
+                ) {
+                    // Apply poison effect if the player is not in a solid block, water, or lava
+                    // i.e. they are in the air/non-solid block that would be affected by the poisonous air
                     player.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 25, 1));
                 }
             }
