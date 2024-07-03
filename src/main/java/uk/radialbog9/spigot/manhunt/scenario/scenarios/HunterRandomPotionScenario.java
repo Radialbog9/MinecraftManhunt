8441f05baa9e19e1b3abbf37b119352a41cbaa97
TheJoeCoder <joe@radialbog9.uk>
2024-07-03 11:15:19 +0100

    Add random potion effect scenario

---
+++ src/main/java/uk/radialbog9/spigot/manhunt/scenario/scenarios/HunterRandomPotionScenario.java
@@ -0,0 +1,28 @@
+package uk.radialbog9.spigot.manhunt.scenario.scenarios;
+
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
+import uk.radialbog9.spigot.manhunt.game.GameManager;
+import uk.radialbog9.spigot.manhunt.scenario.Scenario;
+import uk.radialbog9.spigot.manhunt.scenario.ScenarioRunnable;
+import uk.radialbog9.spigot.manhunt.scenario.utils.ScenarioUtils;
+import uk.radialbog9.spigot.manhunt.utils.Utils;
+
+@Scenario("HUNTER_RANDOM_POTION_EFFECT")
+@ScenarioRunnable
+public class HunterRandomPotionScenario extends BukkitRunnable {
+    @Override
+    public void run() {
+        if (ScenarioUtils.isScenarioEnabled(this)) {
+            for (Player p : GameManager.getGame().getHunters()) {
+                // Pick a random potion effect
+                PotionEffectType[] vals = PotionEffectType.values();
+                PotionEffectType choice = vals[Utils.getRandomInt(0, vals.length - 1)];
+                PotionEffect pe = new PotionEffect(choice, 10, 1);
+                p.addPotionEffect(pe);
+            }
+        }
+    }
+}
