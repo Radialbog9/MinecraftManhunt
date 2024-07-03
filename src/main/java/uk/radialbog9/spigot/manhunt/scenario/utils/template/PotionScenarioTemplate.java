d942db87c34cdc550f10cca77f65ca8210faf803
TheJoeCoder <joe@radialbog9.uk>
2024-07-03 15:58:01 +0100

    Bugfixes for PotionScenarioTemplate

--- src/main/java/uk/radialbog9/spigot/manhunt/scenario/utils/template/PotionScenarioTemplate.java
+++ src/main/java/uk/radialbog9/spigot/manhunt/scenario/utils/template/PotionScenarioTemplate.java
@@ -5,6 +5,7 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 import uk.radialbog9.spigot.manhunt.Manhunt;
+import uk.radialbog9.spigot.manhunt.game.GameManager;
 import uk.radialbog9.spigot.manhunt.scenario.Scenario;
 import uk.radialbog9.spigot.manhunt.scenario.utils.ScenarioUtils;
 import uk.radialbog9.spigot.manhunt.utils.Utils;
@@ -18,11 +19,17 @@ public abstract class PotionScenarioTemplate extends BukkitRunnable {
     @Override
     public void run() {
         String scenarioName = this.getClass().getAnnotation(Scenario.class).value();
-        if (ScenarioUtils.isScenarioEnabled(this)) {
+        if (GameManager.getGame().isGameStarted() &&
+                ScenarioUtils.isScenarioEnabled(this)) {
             for (Player p : getPlayerSet()) {
                 // Pick a random potion effect
-                int rand = Utils.getRandomInt(0, getPotionEffectTypes().size() - 1);
-                PotionEffectType choice = getPotionEffectTypes().get(rand);
+                PotionEffectType choice;
+                if(getPotionEffectTypes().size() > 1) {
+                    int rand = Utils.getRandomInt(0, getPotionEffectTypes().size() - 1);
+                    choice = getPotionEffectTypes().get(rand);
+                } else {
+                    choice = getPotionEffectTypes().get(0);
+                }
 
                 // Create that potion effect
                 int durationSeconds = Manhunt.getInstance().getConfig().getInt("scenarios." + scenarioName + ".duration");
