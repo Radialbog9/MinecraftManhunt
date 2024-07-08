f36c5a179e7175c2012677fd9cfdd5ed45cc8f51
TheJoeCoder <joe@radialbog9.uk>
2024-07-08 22:42:23 +0100

    Start new config... I hope

---
+++ src/main/java/uk/radialbog9/spigot/manhunt/scenario/config/ScenarioConfigurable.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020-2024 Radialbog9/TheJoeCoder and contributors.
+ * You are allowed to use this code under the GPL3 license, which allows
+ * commercial use, distribution, modification, and licensed works,
+ * providing that you distribute your code under the same or similar license.
+ */
+
+package uk.radialbog9.spigot.manhunt.scenario.config;
+
+public interface ScenarioConfigurable {
+    ScenarioConfiguration getConfig();
+    void setConfig(ScenarioConfiguration config);
+}
---
+++ src/main/java/uk/radialbog9/spigot/manhunt/scenario/config/ScenarioConfiguration.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2020-2024 Radialbog9/TheJoeCoder and contributors.
+ * You are allowed to use this code under the GPL3 license, which allows
+ * commercial use, distribution, modification, and licensed works,
+ * providing that you distribute your code under the same or similar license.
+ */
+
+package uk.radialbog9.spigot.manhunt.scenario.config;
+
+import de.exlll.configlib.Configuration;
+
+/**
+ * Configuration class for scenarios.
+ * This class is empty because it is only used to extend by classes.
+ */
+@Configuration
+public class ScenarioConfiguration { }
