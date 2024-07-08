42c078fd925301d2144dce1715876d65327c6a57
TheJoeCoder <joe@radialbog9.uk>
2024-07-08 20:57:41 +0100

    Update copyright notice for all files

--- src/main/java/uk/radialbog9/spigot/manhunt/commands/LeaderboardCommand.java
+++ src/main/java/uk/radialbog9/spigot/manhunt/commands/LeaderboardCommand.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020-2024 Radialbog9/TheJoeCoder and contributors.
+ * You are allowed to use this code under the GPL3 license, which allows
+ * commercial use, distribution, modification, and licensed works,
+ * providing that you distribute your code under the same or similar license.
+ */
+
 package uk.radialbog9.spigot.manhunt.commands;
 
 import cloud.commandframework.annotations.Argument;
--- src/main/java/uk/radialbog9/spigot/manhunt/listeners/ManhuntEventHandler.java
+++ src/main/java/uk/radialbog9/spigot/manhunt/listeners/ManhuntEventHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2023 Radialbog9/TheJoeCoder and contributors.
+ * Copyright (c) 2020-2024 Radialbog9/TheJoeCoder and contributors.
  * You are allowed to use this code under the GPL3 license, which allows
  * commercial use, distribution, modification, and licensed works,
  * providing that you distribute your code under the same or similar license.
