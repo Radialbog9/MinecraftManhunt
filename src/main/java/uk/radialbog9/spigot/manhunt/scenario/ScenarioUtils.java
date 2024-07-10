fa8c45102dde9bac815338a3e36002d4bf0ee9a3
TheJoeCoder <joe@radialbog9.uk>
2024-07-10 23:18:28 +0100

    Try work on serialization to/from something

--- src/main/java/uk/radialbog9/spigot/manhunt/scenario/ScenarioUtils.java
+++ src/main/java/uk/radialbog9/spigot/manhunt/scenario/ScenarioUtils.java
@@ -8,6 +8,10 @@
 package uk.radialbog9.spigot.manhunt.scenario;
 
 import uk.radialbog9.spigot.manhunt.game.GameManager;
+import uk.radialbog9.spigot.manhunt.scenario.config.ScenarioConfiguration;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class ScenarioUtils {
     /**
@@ -18,5 +22,35 @@ public class ScenarioUtils {
     public static boolean isScenarioEnabled(Object instance) {
         String scenarioName = instance.getClass().getAnnotation(Scenario.class).value();
         return GameManager.getGame().isGameStarted() && GameManager.getGame().getActiveScenarios().contains(scenarioName);
+    }
+
+    public static String toConfig(ScenarioConfiguration config) {
+        Method[] methods = config.getClass().getDeclaredMethods();
+        StringBuilder builder = new StringBuilder();
+        for(Method method : methods) {
+            if(method.getName().startsWith("get")) {
+                try {
+                    builder.append(method.getName().substring(3)).append(":").append(method.invoke(config));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return builder.toString();
+    }
+
+    public static ScenarioConfiguration fromConfig(String config, ScenarioConfiguration instance) {
+        String[] lines = config.split(";");
+        for(String line : lines) {
+            String[] parts = line.split(":");
+            try {
+                Field field = instance.getClass().getField(parts[0]);
+                field.setAccessible(true);
+                field.set(instance, parts[1]);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return instance;
     }
 }
