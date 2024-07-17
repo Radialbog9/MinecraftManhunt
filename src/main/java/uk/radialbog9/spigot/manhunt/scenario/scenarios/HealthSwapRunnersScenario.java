1e29fa2fa3e9231e92e9ea34fb873f4cd7ba11cb
TheJoeCoder <joe@radialbog9.uk>
2024-07-17 19:58:28 +0100

    Fix HealthSwapRunnersScenario

--- src/main/java/uk/radialbog9/spigot/manhunt/scenario/scenarios/HealthSwapRunnersScenario.java
+++ src/main/java/uk/radialbog9/spigot/manhunt/scenario/scenarios/HealthSwapRunnersScenario.java
@@ -1,8 +1,8 @@
 /*
  * Copyright (c) 2020-2024 Radialbog9/TheJoeCoder and contributors.
- *  You are allowed to use this code under the GPL3 license, which allows
- *  commercial use, distribution, modification, and licensed works,
- *  providing that you distribute your code under the same or similar license.
+ * You are allowed to use this code under the GPL3 license, which allows
+ * commercial use, distribution, modification, and licensed works,
+ * providing that you distribute your code under the same or similar license.
  */
 
 package uk.radialbog9.spigot.manhunt.scenario.scenarios;
@@ -37,6 +37,6 @@ public class HealthSwapRunnersScenario extends SwapHealthTemplate implements Sce
 
     @Override
     public List<Player> getPlayerSet() {
-        return GameManager.getGame().getHunters();
+        return GameManager.getGame().getRunners();
     }
 }
